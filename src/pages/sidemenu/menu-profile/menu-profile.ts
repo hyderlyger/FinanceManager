3fa8c980779af5c9a4954eda6111287339f61f02
Haider Rasool <hyder.lyger@gmail.com>
2017-05-10 03:39:17 -0700

    Menu Profile done

--- src/pages/sidemenu/menu-profile/menu-profile.ts
+++ src/pages/sidemenu/menu-profile/menu-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DBProvider } from '../../../providers/db-provider'
 
 @IonicPage()
 @Component({
@@ -7,14 +8,23 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'menu-profile.html',
 })
 export class MenuProfile {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  _username : string;
+  _email : string;
+  _actualpass : string;
+  _newpass : string;
+  _newpass2 : string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private dbprovider : DBProvider) {
+    this._username = "";
+    this._email = "";
+    this._actualpass = "";
+    this._newpass = "";
+    this._newpass2 = "";
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MenuProfile');
-  }
-  goback(){
-    this.navCtrl.pop();
+
+    this._username = this.dbprovider.user.name;
+    this._email = this.dbprovider.user.email;
   }
 }
